9e7f03abaa8f12526bc897a211a67c650312ead2
Tobias Kwant <t.kwant1@gmail.com>
2021-06-01 23:42:48 +0200

    added dozzle, renamed env variables

--- startall.sh
+++ startall.sh
@@ -4,3 +4,5 @@ docker-compose --env-file ./nginx/.env -f nginx/docker-compose.yml up -d
 echo "started website"
 docker-compose --env-file ./nextcloud/.env -f nextcloud/docker-compose.yml up -d
 echo "started nextcloud"
+docker-compose --env-file ./dozzle/.env  -f dozzle/docker-compose.yml up -d
+echo "started dozzle"
